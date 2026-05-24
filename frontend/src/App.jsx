bcc7b1a78c8d3d3ccf84d419e25a54755591880f
vviveksharma <visharma@progress.com>
2026-05-24 18:49:08 +0530

    Add ScrollToTop component to fix page navigation scroll issue
    - Create ScrollToTop component that resets scroll position on route change
    - Integrate ScrollToTop in App.jsx
    - Fixes issue where navigating to /download or /docs starts at wrong scroll position
    - All page transitions now start at top

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,18 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Docs from './pages/Docs'
 import Download from './pages/Download'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/docs" element={<Docs />} />
-      <Route path="/download" element={<Download />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/docs" element={<Docs />} />
+        <Route path="/download" element={<Download />} />
+      </Routes>
+    </>
   )
 }
 
